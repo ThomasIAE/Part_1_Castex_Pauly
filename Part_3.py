b2b27a4b851e594287ce14433fe3ea6adc63741b
Thomas PAULY <thomas.pauly@iae-toulouse.fr>
2017-02-28 20:19:16 +0100

    Perform bond profitability test

--- Part_3.py
+++ Part_3.py
@@ -1,10 +1,38 @@
-class Investors(object):
-    """ this is a bond"""
+import string
+import random
+random.choice(string.ascii_letters)
 
-    def __init__(self,aggressive, defensive, mixed):
-        self.aggressive = aggressive
-        self.defensive = defensive
-        self.mixed = mixed
+class investors(object):
 
-    class aggresive(Investors):
-        def calculate_(self, term, investment, interestrate):
+
+class agressive(investors):
+    def __init__(self, investment, type, date_begin):
+        self.investment = investment
+        self.type = type
+        self.date_begin = date_begin
+
+class defensive(self, investment, type, date_begin):
+    def __init__(self, investment, type, date_begin):
+        self.investment = investment
+        self.type = type
+        self.date_begin = date_begin
+
+class mixed (self, investment, type, date_begin):
+    def __init__(self, investment, type, date_begin):
+        self.investment = investment
+        self.type = type
+        self.date_begin = date_begin
+
+while defensive.investment > 1000
+    investment = (1/2) * stock + (1/2) * bond
+
+while agressive.investment > 100
+
+while mixed.investment > 1000
+
+
+
+
+def profitability(self, budget, interest_rate):
+    bond2_return.append(bond2.calculate_return(y, 12000, 0.03))
+
